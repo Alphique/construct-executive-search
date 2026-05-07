5a7f569f58e8687f63e3b7361d610ccaccc1c0b2
LittleCoderMae <mervisnkubo@gmail.com>
2026-05-07 11:27:35 +0200

    updated app.py

--- run.py
+++ run.py
@@ -1,11 +1,7 @@
 # FILE: run.py
 from app import create_app
-from app.core.extensions import db
 
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        # This will now work because create_app() has finished
-        db.create_all()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
